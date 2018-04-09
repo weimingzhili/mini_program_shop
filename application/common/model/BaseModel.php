61425db0c407782656c0f2994425778964adebb5
weimingzhili <weimingzhili@gmail.com>
2018-04-10 07:46:54 +0800

    在 BaseModel 中引入软删除和开启时间戳自动写入

--- application/common/model/BaseModel.php
+++ application/common/model/BaseModel.php
@@ -4,12 +4,22 @@ namespace app\common\model;
 
 use think\Config;
 use think\Model;
+use traits\model\SoftDelete;
 
 /**
  * 基础模型
  */
 class BaseModel extends Model
 {
+    // 引入软删除
+    use SoftDelete;
+
+    /**
+     * 开启时间戳自动写入
+     * @var bool
+     */
+    protected $autoWriteTimestamp = true;
+
     /**
      * image url 获取器
      * @param string $image_url
