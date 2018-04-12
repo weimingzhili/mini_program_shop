5893686f1981dd4159d0993a81cba692485158bc
weimingzhili <weimingzhili@gmail.com>
2018-04-13 07:39:12 +0800

    新增正整数自定义验证规则

--- application/common/validate/Base.php
+++ application/common/validate/Base.php
@@ -10,5 +10,16 @@ use think\Validate;
  */
 class Base extends Validate
 {
-
+    /**
+     * 正整数验证规则
+     * @param mixed $value 验证参数
+     * @param string $rule 验证规则
+     * @param array $data 所有的验证参数
+     * @param string $field 验证的参数名
+     * @return bool|string
+     */
+    protected function positiveInteger($value, $rule, $data, $field)
+    {
+        return is_numeric($value) && is_int($value + 0) && $value > 0 ? true : $field . '必须是正整数';
+    }
 }
