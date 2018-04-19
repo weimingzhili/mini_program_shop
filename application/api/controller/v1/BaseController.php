f21dc5fc285861bd1881eba2f26a8b117ee3d345
WeiZeng <weimingzhili@gmail.com>
2018-04-19 22:22:21 +0800

    去掉 Base controller 废弃的代码

--- application/api/controller/v1/BaseController.php
+++ application/api/controller/v1/BaseController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace app\api\controller\v1;
-use think\Controller;
+
 use app\common\traits\ResponseTrait;
-use think\Response;
+use think\Controller;
 
 /**
  * 基类
@@ -12,22 +12,4 @@ use think\Response;
 class BaseController extends Controller
 {
     use ResponseTrait;
-
-    protected function responses($data, $message, $httpCode)
-    {
-        return Response::create();
-    }
-
-    /**
-     * 输出返回数据
-     * @access protected
-     * @param mixed     $data 要返回的数据
-     * @param String    $type 返回类型 JSON XML
-     * @param integer   $code HTTP状态码
-     * @return Response
-     */
-    protected function response($data, $type = 'json', $code = 200)
-    {
-        return Response::create($data, $type)->code($code);
-    }
 }
